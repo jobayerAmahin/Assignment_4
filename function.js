fef340d65f44dcb1c54714381b35ead9bbb2ae81
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-16 23:12:09 +0300

    Adding Assignment 4 after recheck and discarding the console command from function return

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
-//Task 1
+//-----------------------------------------------------Task 1
 function calculateTax(income, expenses){
     if(income>=0 && expenses>=0 && income>=expenses){
         let tax=(income-expenses)*(20/100);
-        return console.log(tax);
+        return tax;
     }
     else{
-        return console.log("Invalid Input");
+        return "Invalid Input";
     }
 }
 
@@ -15,11 +15,12 @@ function sendNotification(givenEmail){
         let atTheRateIndex=givenEmail.indexOf('@');
         let userName=givenEmail.slice(0,atTheRateIndex);
         let domainName=givenEmail.slice(atTheRateIndex+1,givenEmail.length);
+        let outReturn=`${userName} sent you an email from ${domainName}`;
 
-        return console.log(userName,'sent you an email from',domainName);
+        return outReturn;
     }
     else{
-        return console.log("Invalid Email");
+        return "Invalid Email";
     }
 }
 
@@ -32,16 +33,17 @@ function checkDigitsInName(name){
         }
 
         if(storingNaNoutput.includes(false)){
-            return console.log(true)
+            return true;
         }
         else{
-            return console.log(false)
+            return false;
         }
     }
     else{
-        return console.log("Invalid Input");
+        return "Invalid Input";
     }
 }
+
 
 //-----------------------------------------------------Task 4
 function calculateFinalScore(profileDetails){
@@ -56,23 +58,23 @@ function calculateFinalScore(profileDetails){
                 finalScore=finalScore+20;
             }
             if(finalScore>=80){
-                return console.log(true)
+                return true;
             }
             else{
-                return console.log(false)
+                return false;
             }
         }
         else{
-            return console.log("Invalid Input");
+            return "Invalid Input";
         }
     }
     else{
-        return console.log("Invalid Input");
+        return "Invalid Input";
     }
 
 }
 
-//--------------------------------------------------------Task 5
+//------------------------------------------------------Task 5
 function waitingTime(waitingTimesArr,serialNumber){
     if(typeof(serialNumber)==="number" && Array.isArray(waitingTimesArr)===true){
         let sum=0;
@@ -85,15 +87,17 @@ function waitingTime(waitingTimesArr,serialNumber){
             let remainSerial=serialNumber-givenSerial-1;
             let timeNeed=avgTime*remainSerial;
 
-            return console.log(timeNeed);
+            return timeNeed;
         }
         else{
-            return console.log("Invalid Input")
+            return "Invalid Input";
         }
     }
     else{
-        return console.log("Invalid Input")
+        return "Invalid Input";
     }
 }
+
+
 
 
